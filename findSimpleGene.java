409406e59b05e3ce973f55d9ccb3001fa5cef8e9
Deontee Hendricks <48738169+dhndrx@users.noreply.github.com>
2020-04-15 11:18:19 -0500

    Update findSimpleGene.java

--- findSimpleGene.java
+++ findSimpleGene.java
@@ -1,21 +1,23 @@
 /**
  * Code to find a gene in a DNA string.
- * @author (Deontee Hendricks) 
+ * @author (Deontee Hendricks)
  * @version (1.0)
  */
-public class Part1 {
-    public String findSimpleGene (String dnaString){
-     
+
+public class geneFinder {
+
+    private static String findSimpleGene(String dnaString){
+
         int start = dnaString.indexOf("atg");
         if(start == -1){
             return "";
         }
-        
+
         int end = dnaString.indexOf("taa", start);
         if(end == -1){
             return "";
         }
-        
+
         if((end - start)%3 == 0){
             return dnaString.substring(start,end);
         }
@@ -23,18 +25,22 @@ public class Part1 {
             return "";
         }
     }
-    
-    public void testFindSimpleGene(){
-        
+
+    private static void testFindSimpleGene(){
+
         String [] dnaStrands = {"atgtcactaaatcgaagtaa",
                                 "gatgacaagccgctaggtat",
                                 "tcgtggaaagcggctaacgt",
                                 "atgagaattcgttaaaagct",
                                 "tctcgctgtttccgttcttc"};
-                                
+
         for(String dna : dnaStrands){
             System.out.println("DNA Strand: " + dna);
             System.out.println("Gene: " + findSimpleGene(dna));
         }
+    }
+
+    public static void main(String []args){
+        testFindSimpleGene();
     }
 }
