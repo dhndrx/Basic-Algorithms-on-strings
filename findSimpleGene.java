cdc63962893d2f6bc544431d646d3228e70fb326
Deontee Hendricks <48738169+dhndrx@users.noreply.github.com>
2020-04-17 23:49:37 -0500

    Update findSimpleGene.java

--- findSimpleGene.java
+++ findSimpleGene.java
@@ -1,46 +1,67 @@
 /**
  * Code to find a gene in a DNA string.
- * @author (Deontee Hendricks)
- * @version (1.0)
+ * @author (Deontee Hendricks) 
+ * @version (2.0)
  */
-
-public class geneFinder {
-
-    private static String findSimpleGene(String dnaString){
-
-        int start = dnaString.indexOf("atg");
+public class Part2 {
+    
+    public boolean checkUpperCase(String dnaSequence){
+        return dnaSequence == dnaSequence.toUpperCase();
+    }
+    public String findSimpleGene (String dnaString, String startCodon, String stopCodon){
+        
+        String gene = null;
+        if(checkUpperCase(dnaString)){
+            startCodon = startCodon.toUpperCase();
+            stopCodon = stopCodon.toUpperCase();
+        }
+        
+        else{
+            startCodon = startCodon.toLowerCase();
+            stopCodon = stopCodon.toLowerCase();
+        }
+       
+        int start = dnaString.indexOf(startCodon);
         if(start == -1){
             return "";
         }
-
-        int end = dnaString.indexOf("taa", start);
+        
+        int end = dnaString.indexOf(stopCodon, start);
         if(end == -1){
             return "";
         }
-
+        
         if((end - start)%3 == 0){
-            return dnaString.substring(start,end);
+            if(checkUpperCase(dnaString)){
+                gene = dnaString.substring(start,end+3);
+                return gene.toUpperCase();
+            }
+            else{
+                gene = dnaString.substring(start,end+3);
+                return gene.toLowerCase();
+            }
         }
+        
         else{
             return "";
         }
     }
-
-    private static void testFindSimpleGene(){
-
+    
+    public void testFindSimpleGene(){
+        
+        String startCodon = "atg", stopCodon = "taa";
+        
         String [] dnaStrands = {"atgtcactaaatcgaagtaa",
                                 "gatgacaagccgctaggtat",
-                                "tcgtggaaagcggctaacgt",
+                                "ATGAGAATTCGTTAAAAGCT",
                                 "atgagaattcgttaaaagct",
                                 "tctcgctgtttccgttcttc"};
-
+                                
         for(String dna : dnaStrands){
             System.out.println("DNA Strand: " + dna);
-            System.out.println("Gene: " + findSimpleGene(dna));
+            System.out.println("Gene: " + findSimpleGene(dna, startCodon, stopCodon));
         }
     }
-
-    public static void main(String []args){
-        testFindSimpleGene();
-    }
+    
+    
 }
