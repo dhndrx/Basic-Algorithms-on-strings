792a26d931d53415b352bf5baa436ab0eda79088
Deontee Hendricks <48738169+dhndrx@users.noreply.github.com>
2020-04-18 00:06:50 -0500

    Update youtubeURLfinder.java

--- youtubeURLfinder.java
+++ youtubeURLfinder.java
@@ -9,16 +9,22 @@
 
 import edu.duke.*;
 
-public class urlFinder {
+public class Part4 {
     
     public void printFile(){
+        
         URLResource ur = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+        
         String temp = null;
+        
+        int start = 0, end = 0;
         
         for(String line : ur.lines()){
             temp = line.toLowerCase();
             if(temp.indexOf("youtube")!=-1){
-                System.out.println(line);
+                start = line.indexOf("href=\"");
+                end = line.indexOf("\"",start+6);
+                System.out.println(line.substring(start+6,end));
             }
         }
     }
